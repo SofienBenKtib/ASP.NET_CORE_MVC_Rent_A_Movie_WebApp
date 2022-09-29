f5aaa26603796ce974b2827dcc458f55ae16776b
SofTheDev <sofienbenktib11@gmail.com>
2022-09-29 22:24:55 +0200

    first commit

---
+++ RentAMovie/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿namespace RentAMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ReleaseYear { get; set; }
+        public double Price { get; set; }
+        public bool Available { get; set; }
+        public Movie()
+        {
+
+        }
+    }
+}
